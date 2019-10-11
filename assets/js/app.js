d856376b6a9bd9dc6a0cf86e11d0d7e9ae9ec47a
Rebecca Barrett <barrett4467@gmail.com>
2019-10-10 22:26:58 -0600

    Updated portfolio to include two newest projects and updated styling

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+$("#portfolio").hide();
+$("#contact").hide();
+
+$("#portfolioTop").on("click", function(){
+    $("#portfolio").show();
+})
+
+$("#portfolioSec").on("click", function(){
+    $("#portfolio").show();
+})
+
+$("#contactTop").on("click", function(){
+    $("#contact").show();
+})
+
+$("#contactSec").on("click", function(){
+    $("#contact").show();
+})
+
+
+$("#portfolio-collapse").on("click", function(){
+    $("#portfolio").hide();
+})
+
+$("#contact-collapse").on("click", function(){
+    $("#contact").hide();
+})
